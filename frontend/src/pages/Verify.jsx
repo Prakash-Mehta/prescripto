7b7d251ddd4aa2fc86fc977d5ab871db43223b3d
Prakash Mehta <prakashmehta6200@gmail.com>
2024-10-31 04:03:48 +0530

    final

--- frontend/src/pages/Verify.jsx
+++ frontend/src/pages/Verify.jsx
@@ -16,38 +16,38 @@ const Verify = () => {
     const navigate = useNavigate()
 
     // Function to verify stripe payment
-    const verifyStripe = async () => {
+    //  const verifyStripe = async () => {
 
-        try {
+    //      try {
 
-            const { data } = await axios.post(backendUrl + "/api/user/verifyStripe", { success, appointmentId }, { headers: { token } })
+    //          const { data } = await axios.post(backendUrl + "/api/user/verifyStripe", { success, appointmentId }, { headers: { token } })
 
-            if (data.success) {
-                toast.success(data.message)
-            } else {
-                toast.error(data.message)
-            }
+    //          if (data.success) {
+    //              toast.success(data.message)
+    //          } else {
+    //              toast.error(data.message)
+    //          }
 
-            navigate("/my-appointments")
+    //          navigate("/my-appointments")
 
-        } catch (error) {
-            toast.error(error.message)
-            console.log(error)
-        }
+    //      } catch (error) {
+    //          toast.error(error.message)
+    //          console.log(error)
+    //      }
 
-    }
+    //  }
 
-    useEffect(() => {
-        if (token, appointmentId, success) {
-            verifyStripe()
-        }
-    }, [token])
+    //  useEffect(() => {
+    //      if (token, appointmentId, success) {
+    //          verifyStripe()
+    //      }
+    //  }, [token])
 
-    return (
-        <div className='min-h-[60vh] flex items-center justify-center'>
-            <div className="w-20 h-20 border-4 border-gray-300 border-t-4 border-t-primary rounded-full animate-spin"></div>
-        </div>
-    )
-}
+     return (
+         <div className='min-h-[60vh] flex items-center justify-center'>
+             <div className="w-20 h-20 border-4 border-gray-300 border-t-4 border-t-primary rounded-full animate-spin"></div>
+         </div>
+     )
+ }
 
 export default Verify
