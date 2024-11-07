a6ce69e8a83f7c985e5b62ae0c7f262324b780f4
Prakash Mehta <prakashmehta6200@gmail.com>
2024-11-07 22:19:59 +0530

    production ready

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,12 +1,35 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-
-    mongoose.connection.on('connected', () => console.log("Database Connected"))
-    await mongoose.connect(`${process.env.MONGODB_URL}/prescripto`)
-
+    try {  await mongoose.connect(`${process.env.MONGODB_URL}/prescripto` ,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    mongoose.connection.on('connected', () => console.log("Database Connected"));
+} catch (error) {
+    console.error("Database connection failed", error);
+    process.exit(1); // Exit process with failure code
+}
 }
 
 export default connectDB;
+
+
+
+
+
+
+
+
+// import mongoose from "mongoose";
+
+// const connectDB = async () => {
+
+//     mongoose.connection.on('connected', () => console.log("Database Connected"))
+//     await mongoose.connect(`${process.env.MONGODB_URL}/prescripto`)
+
+// }
+
+// export default connectDB;
 
 // Do not use '@' symbol in your databse user's password else it will show an error.
